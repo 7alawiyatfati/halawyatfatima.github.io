79471f211817e820e96da8d1e4b0f9b33ce00b45
dinushchathurya <dinushchathurya21@gmail.com>
2022-06-07 21:20:22 +0530

    fix: Issue in proxy url order

--- gateway/index.js
+++ gateway/index.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/", proxy("http://localhost:8002"));
-app.use('/customer', proxy("http://localhost:8001"));
-app.use('/shopping', proxy("http://localhost:8003"));
+app.use('/customer', proxy('http://localhost:8001'));
+app.use('/shopping', proxy('http://localhost:8003'));
+app.use('/', proxy("http://localhost:8002"));
 
 app.listen(8000, () => {
   console.log("Gateway is running on port 8000");
